8dac5184adc07c34bf634b5d6f50bc8f8ee23bd0
Orlando <oatr8148@gmail.com>
2026-04-24 11:28:13 +0200

    Actualizacion inserts de SQL y se añadio video Sekiro

---
+++ BASES_DE_DATOS/Scripts_Insercion_Datos/insercion.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO Usuario (Nombre_usuario, Email, Contrasena, Fecha_registro)
+VALUES ('Orlando', 'orlando@arcadepedia.com', 'orlando123', '2026-04-23');
+
+INSERT INTO Usuario (Nombre_usuario, Email, Contrasena, Fecha_registro)
+VALUES ('Jairo', 'jairo@arcadepedia.com', 'jairo123', '2026-04-23');
+
+INSERT INTO Usuario (Nombre_usuario, Email, Contrasena, Fecha_registro)
+VALUES ('Xu', 'xu@arcadepedia.com', 'xu123', '2026-04-23');
+
+INSERT INTO Usuario (Nombre_usuario, Email, Contrasena, Fecha_registro)
+VALUES ('RetroKing', 'retroking@arcadepedia.com', 'retro123', '2026-04-23');
+
+INSERT INTO Usuario (Nombre_usuario, Email, Contrasena, Fecha_registro)
+VALUES ('PixelNova', 'pixelnova@arcadepedia.com', 'pixel123', '2026-04-23');
