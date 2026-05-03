3732e428fd0b5f46ba11991eb33d9c155dc76610
XYJN <xyjn20051205@gmail.com>
2026-05-03 10:55:05 +0200

    Unos cambios sobre Programacion por Jianing Xu Yang

--- PROGRAMACION/src/main/java/proyecto/code/ArcadepediaMain.java
+++ PROGRAMACION/src/main/java/proyecto/code/ArcadepediaMain.java
@@ -1,4 +1,31 @@
 package proyecto.code;
 
-public class ArcadepediaMain {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ArcadepediaMain extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/entrada.fxml"));
+        Parent root = loader.load();
+
+        stage.setScene(new Scene(root));
+
+        stage.setTitle("Arcadepedia");
+        stage.sizeToScene();
+
+        stage.setResizable(false);
+        stage.centerOnScreen();
+
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
