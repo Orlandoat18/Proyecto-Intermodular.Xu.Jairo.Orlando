d1bb0eae9ae3ebd7862de679890660d3740d1c8b
XYJN <xyjn20051205@gmail.com>
2026-05-15 23:46:48 +0200

    Modificacion MC por Jianing Xu Yang

--- PROGRAMACION/src/main/java/proyecto/code/controller/Info_MC_Controller.java
+++ PROGRAMACION/src/main/java/proyecto/code/controller/Info_MC_Controller.java
@@ -5,20 +5,52 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import proyecto.code.exceptions.cargaException;
+import proyecto.code.model.Videojuego;
 import proyecto.code.service.Info_MC_Service;
+import proyecto.code.service.Info_Sekiro_Service;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Info_MC_Controller {
 
+    //Header
     @FXML
     private ImageView logoHeader;
-
     @FXML
     private Button btnInicio;
+
+    //Introduccion
+    @FXML
+    private Text descripcionMC;
+
+    //Table
+    @FXML
+    private Label desarrolladorMC;
+    @FXML
+    private Label distribuidorMC;
+    @FXML
+    private Label diseniadorMC;
+    @FXML
+    private Label plataformaMC;
+    @FXML
+    private Label generoMC;
+    @FXML
+    private Label modoMC;
+
+    @FXML
+    private ImageView mcImage;
+
+    //Personaje
+    @FXML
+    private ImageView steve_personaje;
+    @FXML
+    private Text descripcionP1;
 
     private final Info_MC_Service info_MC_Service = new Info_MC_Service();
 
@@ -27,9 +59,44 @@ public class Info_MC_Controller {
         try {
             info_MC_Service.cargarImagen(logoHeader, "/img/PAGINA_LOGO.jpg");
 
+            info_MC_Service.cargarImagen(mcImage, "/img/minecraft.jpg");
+
+
+
+            cargarDatos();
+
         }catch (cargaException e){
             System.out.println("Error general en carga de recursos");
             System.out.println(e.getMessage());
+        }
+    }
+
+    private void cargarDatos() {
+
+        try {
+
+            Videojuego videojuego = Info_MC_Service.getVideojuego("Minecraft");
+
+            if (videojuego == null) return;
+
+            descripcionMC.setText(videojuego.getDescripcion_Videojuego());
+
+            desarrolladorMC.setText(Info_MC_Service.getDesarrollador(videojuego.getId_Desarrollador()));
+
+            distribuidorMC.setText(Info_MC_Service.getDistribuidores(videojuego.getId_Videojuego()));
+
+            diseniadorMC.setText(Info_MC_Service.getDiseniadores(videojuego.getId_Videojuego()));
+
+            plataformaMC.setText(Info_MC_Service.getPlataformas(videojuego.getId_Videojuego()));
+
+            generoMC.setText(Info_MC_Service.getGeneros(videojuego.getId_Videojuego()));
+
+            modoMC.setText(Info_MC_Service.getModos(videojuego.getId_Videojuego()));
+
+            descripcionP1.setText("Personaje principal del videojuego Sekiro.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
