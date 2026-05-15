cda2b93107a998ed26e5d713b1284dea395e8828
XYJN <xyjn20051205@gmail.com>
2026-05-15 23:57:14 +0200

    Modificacion controller y service de HK por Jianing Xu Yang

--- PROGRAMACION/src/main/java/proyecto/code/controller/Info_HK_Controller.java
+++ PROGRAMACION/src/main/java/proyecto/code/controller/Info_HK_Controller.java
@@ -5,20 +5,51 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import proyecto.code.exceptions.cargaException;
+import proyecto.code.model.Videojuego;
 import proyecto.code.service.Info_HK_Service;
+import proyecto.code.service.Info_MC_Service;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Info_HK_Controller {
-
+    //Header
     @FXML
     private ImageView logoHeader;
-
     @FXML
     private Button btnInicio;
+
+    //Introduccion
+    @FXML
+    private Text descripcionHK;
+
+    //Table
+    @FXML
+    private Label desarrolladorHK;
+    @FXML
+    private Label distribuidorHK;
+    @FXML
+    private Label diseniadorHK;
+    @FXML
+    private Label plataformaHK;
+    @FXML
+    private Label generoHK;
+    @FXML
+    private Label modoHK;
+
+    @FXML
+    private ImageView hkImage;
+
+    //Personaje
+    @FXML
+    private ImageView knight_personaje;
+    @FXML
+    private Text descripcionP1;
 
     private final Info_HK_Service info_HK_Service = new Info_HK_Service();
 
@@ -27,9 +58,43 @@ public class Info_HK_Controller {
         try {
             info_HK_Service.cargarImagen(logoHeader, "/img/PAGINA_LOGO.jpg");
 
+            info_HK_Service.cargarImagen(hkImage, "/img/hollowknight.jpg");
+
+            info_HK_Service.cargarImagen(knight_personaje, "/img/PAGINA_PK.jpg");
+
+            cargarDatos();
         }catch (cargaException e){
             System.out.println("Error general en carga de recursos");
             System.out.println(e.getMessage());
+        }
+    }
+
+    private void cargarDatos() {
+
+        try {
+
+            Videojuego videojuego = Info_MC_Service.getVideojuego("Minecraft");
+
+            if (videojuego == null) return;
+
+            descripcionHK.setText(videojuego.getDescripcion_Videojuego());
+
+            desarrolladorHK.setText(Info_MC_Service.getDesarrollador(videojuego.getId_Desarrollador()));
+
+            distribuidorHK.setText(Info_MC_Service.getDistribuidores(videojuego.getId_Videojuego()));
+
+            diseniadorHK.setText(Info_MC_Service.getDiseniadores(videojuego.getId_Videojuego()));
+
+            plataformaHK.setText(Info_MC_Service.getPlataformas(videojuego.getId_Videojuego()));
+
+            generoHK.setText(Info_MC_Service.getGeneros(videojuego.getId_Videojuego()));
+
+            modoHK.setText(Info_MC_Service.getModos(videojuego.getId_Videojuego()));
+
+            descripcionP1.setText("Personaje principal del videojuego Sekiro.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
