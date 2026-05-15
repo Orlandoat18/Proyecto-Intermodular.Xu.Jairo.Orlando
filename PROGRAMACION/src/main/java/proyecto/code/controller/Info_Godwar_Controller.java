3178f323a37da412ec6b573f014b82dcf56e89ef
XYJN <xyjn20051205@gmail.com>
2026-05-16 00:10:09 +0200

    Modificacion controller de God of war por Jianing Xu Yang

--- PROGRAMACION/src/main/java/proyecto/code/controller/Info_Godwar_Controller.java
+++ PROGRAMACION/src/main/java/proyecto/code/controller/Info_Godwar_Controller.java
@@ -5,21 +5,53 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import proyecto.code.exceptions.cargaException;
+import proyecto.code.model.Videojuego;
 import proyecto.code.service.Info_Godwar_Service;
+import proyecto.code.service.Info_HK_Service;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Info_Godwar_Controller {
 
+    //Header
     @FXML
     private ImageView logoHeader;
-
     @FXML
     private Button btnInicio;
 
+    //Introduccion
+    @FXML
+    private Text descripcionGW;
+
+    //Table
+    @FXML
+    private Label desarrolladorGW;
+    @FXML
+    private Label distribuidorGW;
+    @FXML
+    private Label diseniadorGW;
+    @FXML
+    private Label plataformaGW;
+    @FXML
+    private Label generoGW;
+    @FXML
+    private Label modoGW;
+
+    @FXML
+    private ImageView gwImage;
+
+    //Personaje
+    @FXML
+    private ImageView kratos_personaje;
+    @FXML
+    private Text descripcionP1;
+    
     private final Info_Godwar_Service info_Godwar_Service = new Info_Godwar_Service();
 
     @FXML
@@ -27,9 +59,40 @@ public class Info_Godwar_Controller {
         try {
             info_Godwar_Service.cargarImagen(logoHeader, "/img/PAGINA_LOGO.jpg");
 
+            info_Godwar_Service.cargarImagen(gwImage, "/img/Godofwar.jpg");
+
         }catch (cargaException e){
             System.out.println("Error general en carga de recursos");
             System.out.println(e.getMessage());
+        }
+    }
+
+    private void cargarDatos() {
+
+        try {
+
+            Videojuego videojuego = Info_Godwar_Service.getVideojuego("Minecraft");
+
+            if (videojuego == null) return;
+
+            descripcionGW.setText(videojuego.getDescripcion_Videojuego());
+
+            desarrolladorGW.setText(Info_Godwar_Service.getDesarrollador(videojuego.getId_Desarrollador()));
+
+            distribuidorGW.setText(Info_Godwar_Service.getDistribuidores(videojuego.getId_Videojuego()));
+
+            diseniadorGW.setText(Info_Godwar_Service.getDiseniadores(videojuego.getId_Videojuego()));
+
+            plataformaGW.setText(Info_Godwar_Service.getPlataformas(videojuego.getId_Videojuego()));
+
+            generoGW.setText(Info_Godwar_Service.getGeneros(videojuego.getId_Videojuego()));
+
+            modoGW.setText(Info_Godwar_Service.getModos(videojuego.getId_Videojuego()));
+
+            descripcionP1.setText("Personaje principal del videojuego Sekiro.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
--- PROGRAMACION/src/main/java/proyecto/code/service/Info_Godwar_Service.java
+++ PROGRAMACION/src/main/java/proyecto/code/service/Info_Godwar_Service.java
@@ -2,11 +2,17 @@ package proyecto.code.service;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import proyecto.code.config.DatabaseConfig;
+import proyecto.code.dao.impl.VideojuegoDAOImpl;
 import proyecto.code.exceptions.cargaException;
+import proyecto.code.model.Videojuego;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Info_Godwar_Service {
+    private static final VideojuegoDAOImpl dao = new VideojuegoDAOImpl();
 
     public void cargarImagen(ImageView imageView, String ruta) throws cargaException {
 
@@ -18,5 +24,47 @@ public class Info_Godwar_Service {
 
         Image img = new Image(recurso.toExternalForm());
         imageView.setImage(img);
+    }
+
+    public static Videojuego getVideojuego(String titulo) throws SQLException {
+        Connection connection = DatabaseConfig.getConnection();
+
+        return dao.obtenerVideojuegoPorTitulo(connection,titulo);
+    }
+
+    public static String getDesarrollador(int idDesarrollador) throws SQLException {
+        Connection connection = DatabaseConfig.getConnection();
+
+        return dao.getDesarrollador(connection,idDesarrollador);
+    }
+
+    public static String getDistribuidores(int idVideojuego) throws SQLException {
+        Connection connection = DatabaseConfig.getConnection();
+
+        return dao.getDistribuidor(connection,idVideojuego);
+    }
+
+    public static String getDiseniadores(int idVideojuego) throws SQLException {
+        Connection connection = DatabaseConfig.getConnection();
+
+        return dao.getDiseniador(connection,idVideojuego);
+    }
+
+    public static String getPlataformas(int idVideojuego) throws SQLException {
+        Connection connection = DatabaseConfig.getConnection();
+
+        return dao.getPlataforma(connection,idVideojuego);
+    }
+
+    public static String getGeneros(int idVideojuego) throws SQLException {
+        Connection connection = DatabaseConfig.getConnection();
+
+        return dao.getGenero(connection,idVideojuego);
+    }
+
+    public static String getModos(int idVideojuego) throws SQLException {
+        Connection connection = DatabaseConfig.getConnection();
+
+        return dao.getModo(connection,idVideojuego);
     }
 }
